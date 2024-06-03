6bd72edc41f5cfc97237d47fb649b1360f2a6cd0
milky VVay <maks.http@mail.ru>
2024-06-03 14:25:43 +0300

    Close #18 Fix ResponisiveView

--- src/features/ResponsiveView.ts
+++ src/features/ResponsiveView.ts
@@ -1,25 +1,34 @@
 import { useState, useEffect } from 'react';
 
-function getWindowDimensions(): { width: number, height: number } {
-  const { innerWidth: width, innerHeight: height } = window;
-  
-  return {
-    width,
-    height
-  };
-}
+const screenSizes = { 
+    desktop: {width: 1300, name: "DESKTOP"},
+    bigTablet: {width: 1000, name: "BIG_TABLET"}, 
+    smallTablet: {width: 768, name: "SMALL_TABLET"}, 
+    bigMobile: {width: 480, name: "BIG_MOBILE"}, 
+    smallMobile: {width: 320, name: "SMALL_MOBILE"} 
+};
 
-export default function ResponsiveView(): { width: number, height: number } {
-  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
+export default function HandleResponsiveView(): string {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
   useEffect(() => {
     function handleResize(): void {
-      setWindowDimensions(getWindowDimensions());
+      setWindowWidth(window.innerWidth);
     }
 
     window.addEventListener('resize', handleResize);
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  return windowDimensions;
+  if (windowWidth >= screenSizes.desktop.width) {
+    return screenSizes.desktop.name;
+  } else if ((windowWidth < screenSizes.desktop.width) && (windowWidth >= screenSizes.bigTablet.width)) {
+    return screenSizes.bigTablet.name;
+  } else if ((windowWidth < screenSizes.bigTablet.width) && (windowWidth >= screenSizes.smallTablet.width)) {
+    return screenSizes.smallTablet.name;
+  } else if ((windowWidth < screenSizes.smallTablet.width) && (windowWidth >= screenSizes.bigMobile.width)) {
+    return screenSizes.bigMobile.name;
+  } else {
+    return screenSizes.smallMobile.name;
+  }
 }
